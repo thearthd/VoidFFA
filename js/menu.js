77fabc0d5d449e3444036e59f6057eeb6d83fe26
thearthd <verythegamer2@gmail.com>
2025-07-20 14:53:58 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -948,7 +948,7 @@ let playButton = createAnimatedButton(
 let settingsButton = createAnimatedButton(
     "https://codehs.com/uploads/b3e2a8dfe6107e2af96ce74f9799b0f8",
     1920 / 8, 1080 / 8,
-    25, getHeight() / 2 - leftbuttonSpacing / 2, // Position below Games
+    25, getHeight() / 2 - leftbuttonSpacing, // Position below Games
     1920 / 8, 1080 / 10,
     () => {
         console.log("Settings button hit");
@@ -972,7 +972,7 @@ let careerButton = createAnimatedButton(
 let loadoutButton = createAnimatedButton(
     "https://codehs.com/uploads/765a0c87dc6d5d571ff25f139003227f",
     1920 / 8, 1080 / 8,
-    25, getHeight() / 2 + leftbuttonSpacing * 1.5, // Position below Career
+    25, getHeight() / 2 + leftbuttonSpacing, // Position below Career
     1920 / 8, 1080 / 10,
     () => {
         console.log("Loadout button hit");
