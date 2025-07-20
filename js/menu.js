6ad7c85dec2e21864c42bfdc9ee672fc1fe829e9
thearthd <verythegamer2@gmail.com>
2025-07-20 16:19:01 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1007,7 +1007,7 @@ let gamesButton = createAnimatedButton(
 let updateBoard = createAnimatedButton(
     "https://codehs.com/uploads/9323bdb40e74869eebd229ddd37ba098", // Provided games button image
     1080/3, 1440/3,
-    getWidth() - (1080/3), getHeight()/2, // Position below Play
+    getWidth() - (1080/3)*2, getHeight()/2, // Position below Play
     1920 / 6 - 25, 1080 / 8,
     () => {
         console.log("updateBoard button hit");
