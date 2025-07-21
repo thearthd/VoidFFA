1fc66b664535bdcaf21a0576accdfbe4369d0f25
thearthd <verythegamer2@gmail.com>
2025-07-21 09:18:16 -0400

    Update weapons.js

--- js/weapons.js
+++ js/weapons.js
@@ -14,7 +14,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { Loader } from './Loader.js';
 
 
-// f
+// ff
 
 
 const scopeOverlay = document.getElementById('scopeOverlay');
