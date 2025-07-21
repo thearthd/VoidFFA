4d8f9a328a9621647df74d2fb8e4a544f2ae8ae6
thearthd <verythegamer2@gmail.com>
2025-07-21 08:57:12 -0400

    Update soundConfig.js

--- js/soundConfig.js
+++ js/soundConfig.js
@@ -23,7 +23,12 @@ export const SOUND_CONFIG = {
         'reloadEnd': { volume: 0.5, hearingRange: 25, rolloffFactor: 2, distanceModel: 'linear' },
         'pull': { volume: 0.5, hearingRange: 25, rolloffFactor: 2, distanceModel: 'linear' }
     },
-
+    'm79': {
+        'shot': { volume: 1, hearingRange: 55, rolloffFactor: 2, distanceModel: 'linear' },
+        'reloadStart': { volume: 0.5, hearingRange: 25, rolloffFactor: 2, distanceModel: 'linear' },
+        'reloadEnd': { volume: 0.5, hearingRange: 25, rolloffFactor: 2, distanceModel: 'linear' },
+        'pull': { volume: 0.5, hearingRange: 25, rolloffFactor: 2, distanceModel: 'linear' }
+    },
     // Physics Sounds (example)
     'footstep': {
         'run': { volume: 0.5, hearingRange: 35, rolloffFactor: 1.5, distanceModel: 'linear' },
