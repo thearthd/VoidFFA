a01bd9263526f1773f18266cc8e2bf04c61c85a4
thearthd <verythegamer2@gmail.com>
2025-07-24 16:26:11 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1134,7 +1134,7 @@ disclaimerText.setPosition(getWidth()/2, getHeight()-100);
 
 let escMenu = new ImageShape("https://codehs.com/uploads/badb165ef3d765a60258ba9db41f3f28");
 escMenu.setSize(1920/2, 1080/2);
-escMenu.setPosition(getWidth()/2 - (1920/2), getHeight()/2 - (1080/2));
+escMenu.setPosition(getWidth()/2 - (1920/4), getHeight()/2 - (1080/4));
 
 // Make sure the pause menu is initially hidden
 
