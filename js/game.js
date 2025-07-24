86f863d388b205921446695d62e4c68d0a85660b
thearthd <verythegamer2@gmail.com>
2025-07-24 13:07:14 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -566,7 +566,7 @@ export async function startGame(username, mapName, initialDetailsEnabled, ffaEna
   if (ffaEnabled) {
     gameTimerElement.style.display = 'block';
 
-    const INITIAL_DURATION = 10 * 60;
+    const INITIAL_DURATION = 1 * 60;
     let currentRemainingSeconds = null;
     let gameEnded = false;
     let ownerInterval = null;
