3712011f7b95f7fcd93a6b9812076f97115a6a05
thearthd <verythegamer2@gmail.com>
2025-07-25 21:07:08 -0400

    Update physics.js

--- js/physics.js
+++ js/physics.js
@@ -362,7 +362,7 @@ _stepUpIfPossible() {
      * @param {number} delta The fixed time step for this physics update.
      */
 _updatePlayerPhysics(delta) {
-        this._simpleStepUp();
+        this._stepUpIfPossible();
     // Store previous grounded state and reset for this frame
     const wasGrounded = this.isGrounded;
     this.isGrounded = false;
