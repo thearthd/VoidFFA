379866caaf6f2bde60a504de827944897fa2aba9
thearthd <verythegamer2@gmail.com>
2025-08-03 14:22:04 -0400

    Update weapons.js

--- js/weapons.js
+++ js/weapons.js
@@ -732,8 +732,7 @@ update(inputState, delta, playerState) {
     if (this.currentKey === "ak-47") {
       if (shotIndex >= 3) {
         const decayFactor = 0.6; // Adjust this value to change the rate of decay
-        const baseRecoil = 0.018; // The starting recoil at shotIndex 6
-        const recoilDecay = baseRecoil * Math.pow(decayFactor, shotIndex - 6);
+        const recoilDecay =  appliedRecoilAngle * Math.pow(decayFactor, shotIndex - 6);
         appliedRecoilAngle = recoilDecay * recoilMultipler;
       }
     }
@@ -741,8 +740,7 @@ update(inputState, delta, playerState) {
     if (this.currentKey === "viper") {
       if (shotIndex >= 3) {
         const decayFactor = 0.6; // Adjust this value to change the rate of decay
-        const baseRecoil = 0.028; // The starting recoil at shotIndex 6
-        const recoilDecay = baseRecoil * Math.pow(decayFactor, shotIndex - 6);
+        const recoilDecay =  appliedRecoilAngle * Math.pow(decayFactor, shotIndex - 6);
         appliedRecoilAngle = recoilDecay * recoilMultipler;
       }
     }
