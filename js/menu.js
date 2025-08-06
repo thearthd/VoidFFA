fedb56d2b254368bf28023255c8f01cc7d8aabeb
thearthd <verythegamer2@gmail.com>
2025-08-06 12:39:04 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1653,15 +1653,13 @@ let chatListener = null;
 function createMenuChatElements() {
   hud.style.display = 'flex';
   hud.style.position = 'absolute';
-
-  const chatBox = document.createElement("chat-box");
   document.body.appendChild(chatBox); // so it has dimensions
 
   const width = chatBox.offsetWidth;
   const height = chatBox.offsetHeight;
 
-  hud.style.left = `calc(50% - ${width / 2}px)`;
-  hud.style.top = `calc(50% - ${height / 2}px)`;
+  hud.style.left = `calc(50% + ${width / 2}px)`;
+  hud.style.top = `calc(50% + ${height / 2}px)`;
 }
 
 let chatCooldown = false;
