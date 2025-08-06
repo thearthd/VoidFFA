144d71c6355a8a1406cc015f030afdee5a5fab3c
thearthd <verythegamer2@gmail.com>
2025-08-06 13:44:12 -0400

    Update bannedWords.js

--- js/bannedWords.js
+++ js/bannedWords.js
@@ -1,22 +1,45 @@
 export const bannedWords = [
-    // Core swears
-    "fuck", "fuk", "f*ck", "f.u.c.k",
-    "shit", "sh1t", "sh!t",
-    "bitch", "biatch", "b!tch", "b1tch",
-    "asshole", "a$$hole", "a55hole",
-    "dumbass", "jackass", "smartass", "lazyass",
-    "cunt", "kunt",
-    "dick", "dik", "d!ck",
-    "pussy", "pusy", "pussie",
-    "cock", "c0ck",
-    "whore", "hoe", "h0e",
-    "slut", "slutt", "slvtt",
+    // Core swears & variations
+    "fuck","fuk","f*ck","f.u.c.k","f u c k","fucc","fukk","fux","phuck","f0ck","f0k","focker",
+    "shit","sh1t","sh!t","s.h.i.t","shitt","shiit","shyt","shyte","shiet","shittie","bullshit","bs",
+    "bitch","biatch","b!tch","b1tch","b*tch","b i t c h","bishes","betch","bitchez","b1chez",
 
-    // Slurs + variations
-    "nigger", "nigga", "n1gger", "n1gga", "ni99er", "ni99a",
-    "faggot", "fag", "fa99ot", "f@ggot", "f@g",
-    "tranny", "retard", "r3tard",
-    "spic", "sp1c",
-    "kike", "chink", "gook", "wetback", "coon", "beaner", "cracker", "honky",
-    "raghead", "sandnigger", "porchmonkey"
+    "cunt","kunt","c*nt","c u n t","cnt","qunt","q*nt","khunt",
+    "dick","dik","d!ck","d1ck","d1k","diq","d!q","d1q","dic","dyke","dildo",
+    "pussy","pusy","pussie","pussee","puzzy","puss","pussay","puss-e","p*ssy",
+    "cock","c0ck","cok","c@ck","cokk","kok","k0k","cox","c0x","coque",
+    "whore","hoe","h0e","hoar","ho3","wh0re","whoar","hore","hoochie","skank","harlot",
+    "slut","slutt","slvtt","sl@t","s1ut","s1utt","s!ut","sk@nk","slag",
+
+    // Racial / ethnic slurs & variations
+    "nigger","nigga","n1gger","n1gga","ni99er","ni99a","niggah","nigguh","nibba","nibber","niger","nigg",
+    "faggot","fag","fa99ot","f@ggot","f@g","fagg","f4g","f4ggot","feg","feget",
+    "tranny","transvestite","shemale","ladyboy","he-she","genderbender",
+    "retard","r3tard","ret@rd","rehtard","retarded",
+    // Removed idiot, moron
+    "spic","sp1c","spik","sp!c",
+    "kike","kyke","kiqe","k!ke","k1ke",
+    "chink","ching","chingchong","ching-chong","chiink","ch1nk",
+    "gook","g00k","g0ok",
+    "wetback","beaner","beener","b33ner","b3aner",
+    "coon","c00n","c0on","co0n",
+    "cracker","cracka","craka","cr@cker","cr@cka",
+    "honky","honkie","h0nky","h0nkie",
+    "raghead","raghed","raghede",
+    "sandnigger","sandnigga","sandn1gga","sandn1gger","sandni99er","sandni99a",
+    "porchmonkey","porchm0nkey","porchm0nky","p0rchmonkey","p0rchm0nkey",
+
+    // Sexual content
+    "cum","cumm","cumming","cumshot","cumslut","cumdump","cumwhore","cumbucket",
+    "jizz","j1zz","j!zz","j1z","j!z",
+    "orgasm","orcasm","orgazm","org@sm",
+    "masturbate","masturbation","masturbating","mastrubate","mastrabate","mastrbate",
+    "jerkoff","jerking","jerk-off","wank","w@nking",
+    // Removed wanker
+    "blowjob","blow-job","blow j0b","bl0wj0b","bl0wjob","bj","b.j.","b jawb",
+    "handjob","hand-job","h@ndjob",
+    "anal","an@l","anus","buttsex","butt-sex","b00tysex",
+    "rimjob","rim-job","rim j0b","r1mjob",
+    "deepthroat","deep-throat","deep throat","dp","d.p.",
+    "porn","p0rn","pr0n","p orn","p*rn","pornhub","xvideos","xnxx",
 ];
