6443dd027906230045f07fda14af201937d3bffb
thearthd <verythegamer2@gmail.com>
2025-08-08 15:44:39 -0400

    Update network.js

--- js/network.js
+++ js/network.js
@@ -1,7 +1,7 @@
 // network.js
 import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/0.152.0/three.module.js";
 
-// New imports for game slot management
+// New imports for game slot management f
 import {
     claimGameSlot,
     releaseGameSlot,
